b22f7592a8d4353022c0989e9f8ab37018f13bfb
Eric Wang <johnericcw@gmail.com>
2019-05-26 19:01:01 -0500

    Change deploy dir name, successfully check out arbitrary remote branch

--- manager/main.js
+++ manager/main.js
@@ -1,8 +1,19 @@
 const fs = require('fs');
+const nodegit = require('nodegit');
 
-const BOT_WORKING_DIR = './bot_working_dir'
+const BOT_DEPLOY_DIR = './bot_deploy_dir'
+const REPO_PATH = 'https://github.com/jiphan/hayasaka.git'
 
-if (!fs.existsSync(BOT_WORKING_DIR)) {
-    fs.mkdirSync(BOT_WORKING_DIR)
+async function checkout_remote_branch(branch) {
+    let repo = await (fs.existsSync(BOT_DEPLOY_DIR) ? nodegit.Repository.open(BOT_DEPLOY_DIR) 
+                                                    : nodegit.Clone(REPO_PATH, BOT_DEPLOY_DIR));
+    
+    // taken from https://github.com/nodegit/nodegit/blob/master/examples/checkout-remote-branch.js
+    return repo.getHeadCommit()
+        .then(head_commit => repo.createBranch(branch, head_commit, false))
+        .then(reference => repo.checkoutBranch(reference))
+        .then(() => repo.getReferenceCommit('refs/remotes/origin/' + branch))
+        .then(commit => nodegit.Reset.reset(repo, commit, 3))
 }
 
+checkout_remote_branch('working').then(() => console.log('complete'))
