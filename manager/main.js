6ef7c47a914a8db29202eb5a13204d60c05424f4
Eric Wang <johnericcw@gmail.com>
2019-05-26 20:50:56 -0500

    nuke the deploy repo every time, quick hack with copying files to get keys.json in hayasaka dir, hayasaka launching successfully from manager

--- manager/main.js
+++ manager/main.js
@@ -1,13 +1,18 @@
+const child_process = require('child_process');
 const fs = require('fs');
 const nodegit = require('nodegit');
+const path = require('path');
+const rimraf = require('rimraf');
 
 const BOT_DEPLOY_DIR = './bot_deploy_dir'
-const REPO_PATH = 'https://github.com/jiphan/hayasaka.git'
 
-async function checkout_remote_branch(branch) {
-    let repo = await (fs.existsSync(BOT_DEPLOY_DIR) ? nodegit.Repository.open(BOT_DEPLOY_DIR) 
-                                                    : nodegit.Clone(REPO_PATH, BOT_DEPLOY_DIR));
-    
+async function checkout_remote_branch_for_bot_deploy(branch) {
+    const REPO_PATH = 'https://github.com/jiphan/hayasaka.git'
+    if (fs.existsSync(BOT_DEPLOY_DIR)) {
+        rimraf.sync(BOT_DEPLOY_DIR);  // rm -rf on bot deploy dir, no need to deal with repo syncing issues
+    }
+
+    let repo = await nodegit.Clone(REPO_PATH, BOT_DEPLOY_DIR);
     // taken from https://github.com/nodegit/nodegit/blob/master/examples/checkout-remote-branch.js
     return repo.getHeadCommit()
         .then(head_commit => repo.createBranch(branch, head_commit, false))
@@ -16,4 +21,17 @@ async function checkout_remote_branch(branch) {
         .then(commit => nodegit.Reset.reset(repo, commit, 3))
 }
 
-checkout_remote_branch('working').then(() => console.log('complete'))
+async function launch_bot(bot) {
+    await checkout_remote_branch_for_bot_deploy('feature/master/bot_manager');
+    
+    const bot_dir = path.join(BOT_DEPLOY_DIR, bot);
+    fs.copyFileSync('./keys.json', path.join(bot_dir, 'keys.json'));  // TODO: this is stupid, just take the path in via command line and pass it on
+    child_process.fork(path.join(bot_dir, 'main.js'));
+}
+
+launch_bot('hayasaka');
+
+setInterval(() => console.log('hi'), 1000);
+
+// Goal 2: make manager an actual bot to make interacting easy, still launch hayasaka on manager startup
+// Goal 3: let manager take reboot command to restart hayasaka with new code (hard code to master for now)
