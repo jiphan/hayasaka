c5c1a65a702d7867ff0cf4477269acf0028eddab
James Phan <jiphan@umich.edu>
2019-05-27 16:57:58 -0500

    added gsmatch(), thanks, and cleaned msg_parse()

--- hayasaka/add.js
+++ hayasaka/add.js
@@ -8,7 +8,6 @@ import { google } from 'googleapis';
  * @param   {string}    spreadsheet_dst     Location in document to append
  */
 export async function gsappend(gClient, row, spreadsheet_id, spreadsheet_dst){
-    row.pop();
     const gsapi = google.sheets({version:'v4', auth: gClient })
     const update = {
         spreadsheetId: spreadsheet_id,
@@ -16,25 +15,24 @@ export async function gsappend(gClient, row, spreadsheet_id, spreadsheet_dst){
         valueInputOption: 'USER_ENTERED',
         resource: { values: [ row ], }
     };
-    let res = await gsapi.spreadsheets.values.append(update);
-    // console.log(res.statusText);
+    gsapi.spreadsheets.values.append(update);
 }
 
 /**
  * Parses a discord message into three fields
  * @param   {string}    args    Discord message to be parsed
- * @returns {string[]}          [title, source, separator]
+ * @returns {string[]}          [title, separator, source]
  */
 export function msg_parse(args) {
     let i = what_div(args);
     if(i === -1) return [args.slice(1).join(' '), '', ''];
     let title = args.slice(1, i).join(' ');
     let source = args.slice(i + 1).join(' ');
-    return [title, source, args[i]];    // [stand by me, sarazanmai, from]
+    return [title, args[i], source];    // [stand by me, from, sarazanmai]
 }
 
 function what_div(args) {
-    let div = ['from', 'by', '|'];
+    let div = ['from', 'by', 'of', '|'];
     let divi = ''
     args.forEach(a =>{
         div.forEach(d =>{
--- hayasaka/main.js
+++ hayasaka/main.js
@@ -19,15 +19,40 @@ session.dClient.on('message', msg=>{
             msg.channel.send('pong!');
             break;
         case 'sheet':
-            msg.reply('https://docs.google.com/spreadsheets/d/1V1TXuBip74BkEXaP20XDW7-uxa7B8xrRZpyhLMRlH8A');
+            msg.reply(`https://docs.google.com/spreadsheets/d/${spreadsheet_id}`);
             break;
         case 'add':
             let row = add.msg_parse(args);
-            msg.reply(`**${row[0]}** ${row[2]} ${row[1]}... got it!`);
+            let div = row.splice(1, 1);
+            msg.reply(`**${row[0]}** ${div} ${row[1]}`.trim() + '... got it!');
             add.gsappend(session.gClient, row, spreadsheet_id, spreadsheet_dst);
+            gsmatch(session.gClient, row, msg);
+            break;
+        case 'thanks':
+            msg.reply('happy to help!');
             break;
         case 'help':
-            msg.reply('you can !ping, !sheet, and !add. More info on how to use !add coming soon ~');
+            msg.reply('you can !ping, !sheet, !add, and !thanks. More info on how to use !add coming soon ~');
             break;
     }
 })
+
+// kill me please
+import { google } from 'googleapis';
+function gsmatch(gClient, row, msg) {
+    const gsapi = google.sheets({version:'v4', auth: gClient })
+    gsapi.spreadsheets.values.get({
+        spreadsheetId: spreadsheet_id,
+        range: spreadsheet_dst,
+        majorDimension: 'ROWS'
+    }, function(err, response) {
+        let match = false
+        console.log(response.data.values);
+        response.data.values.forEach(a =>{
+            if(a[0] === row[0]) {
+                if(a[1] === row[1]) match = true;
+            }
+        });
+        if(match) msg.channel.send('hey I know that song!');
+    });
+}
