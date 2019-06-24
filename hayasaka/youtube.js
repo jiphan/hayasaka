3ba174862edbbe049fc4e4873c56f41da4b68f2d
James Phan <jiphan@umich.edu>
2019-06-24 13:04:19 -0400

    added yt playlist functions for viewing and queueing

--- hayasaka/youtube.js
+++ hayasaka/youtube.js
@@ -36,3 +36,55 @@ export async function get_info(yClient, id, info) {
             break;
     }
 }
+
+export async function add_video(yClient, playlist_id, video_id, recents) {
+    const gsapi = google.youtube({version: 'v3', auth: yClient});
+    gsapi.playlistItems.insert({
+        part: "snippet",
+        resource: {
+            snippet: {
+                playlistId: playlist_id,
+                resourceId: {
+                    kind: "youtube#video",
+                    videoId: YouTubeGetID(video_id)
+                }
+            }
+        }
+    })
+    recents.push([await get_title(video_id), `http://youtu.be/${YouTubeGetID(video_id)}`]);
+    if(recents.length > 5) recents.splice(0, 1);
+    return recents;
+}
+
+export async function get_playlist(yClient, playlist_id) {
+    const gsapi = google.youtube({version: 'v3', auth: yClient});
+    let result = await gsapi.playlistItems.list({
+        part: 'snippet,contentDetails',
+        playlistId: playlist_id
+    });
+    let clean = [];
+    result.data.items.forEach(async a=> {
+        clean.push([decode(a.snippet.title), `http://youtu.be/${a.contentDetails.videoId}`]);
+    });
+    return clean;
+}
+
+/**
+* Get YouTube ID from various YouTube URL
+* @author: takien
+* @url: http://takien.com
+* For PHP YouTube parser, go here http://takien.com/864
+*/
+function YouTubeGetID(url){
+    var ID = '';
+    url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+    if(url[2] !== undefined) {
+      ID = url[2].split(/[^0-9a-z_\-]/i);
+      ID = ID[0];
+    }
+    else {
+      ID = url;
+    }
+      return ID;
+  }
+
